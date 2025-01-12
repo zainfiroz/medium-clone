6ce1e6b25b5710f89f33b24d3379211a660ebde3
Zain-firoz <zainfiroz007@gmail.com>
2025-01-12 21:19:34 +0530

    Type Errors

--- frontend/src/components/BlogCard.tsx
+++ frontend/src/components/BlogCard.tsx
@@ -45,7 +45,7 @@ export const BlogCard = ({
         </Link>
 }
 
-export function Avatar({ name, size = "small" }: { name: string , size: "small" | "big" }) {
+export function Avatar({ name, size = "small" }: { name: string , size?: "small" | "big" }) {
     return (
         <div className={`relative inline-flex items-center justify-center overflow-hidden 
     bg-gray-600 rounded-full ${size === "small" ? "w-6 h-6" : "w-8 h-8"}`}>
